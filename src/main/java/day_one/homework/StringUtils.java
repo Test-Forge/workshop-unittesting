3ac6f47d3a646f5fced0fba35b981843800dde6d
Stanislav Scurtu <stanislav.scurtu@gmail.com>
2024-05-16 13:53:02 +0300

    homework day1

---
+++ src/main/java/day_one/homework/StringUtils.java
@@ -0,0 +1,32 @@
+package day_one.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+
+    public static String reverseString(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static String removeVowels(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.replaceAll("[aeiouAEIOU]", "");
+    }
+
+    public static List<String> splitAndRemoveEmptyStrings(String str) {
+        if (str == null || str.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return Arrays.stream(str.split(","))
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
+    }
+}
